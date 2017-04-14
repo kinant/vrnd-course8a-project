a8be42b62dd8626ae2c623228157adb790fe2c1b
Kinan Turman <kinan.t@gmail.com>
2017-04-14 03:34:12 -0600

    First Commit
    Created the ControllerInputManager script to manage controller input. Created a separate Teleporter script to create the teleporter + laser on left controller.

---
+++ Assets/Scripts/ControllerInputManager.cs
@@ -0,0 +1,198 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ControllerType
+{
+    Left, Right
+}
+
+public struct InputEventArgs
+{
+    public SteamVR_Controller.Device controller;
+    public ControllerType controllerType;
+    public float padX, padY;
+}
+
+public delegate void InputEventHandler(InputEventArgs e);
+
+public class ControllerInputManager : MonoBehaviour {
+
+    public ControllerType controllerType;
+
+    private string contTypeString {
+        get {
+            if (controllerType == ControllerType.Left)
+            {
+                return "left";
+            }
+            else {
+                return "right";
+            }
+        }
+    }
+
+    private SteamVR_TrackedObject trackedObj;
+
+    public event InputEventHandler TriggerUp;
+    public event InputEventHandler TriggerDown;
+    public event InputEventHandler TouchPadPressed;
+    public event InputEventHandler TouchPadTouchUp;
+    public event InputEventHandler TouchPadTouchDown;
+    public event InputEventHandler TouchPadPressUp;
+    public event InputEventHandler TouchPadPressDown;
+
+    public bool isTriggerPressed = false;
+    public bool isTouchPadTouched = false;
+    public bool isTouchPadPressed = false;
+
+    private SteamVR_Controller.Device device {
+        get { return SteamVR_Controller.Input((int) trackedObj.index); }
+    }
+
+    public virtual void OnTriggerUp(InputEventArgs e) {
+        if (TriggerUp != null) {
+            TriggerUp(e);
+        }
+    }
+
+    public virtual void OnTriggerDown(InputEventArgs e) {
+        if (TriggerDown != null) {
+            TriggerDown(e);
+        }
+    }
+
+    public virtual void OnTouchPadTouchedDown(InputEventArgs e)
+    {
+        if (TouchPadTouchDown != null)
+        {
+            TouchPadTouchDown(e);
+        }
+    }
+
+    public virtual void OnTouchPadTouchedUp(InputEventArgs e)
+    {
+        if (TouchPadTouchUp != null)
+        {
+            TouchPadTouchUp(e);
+        }
+    }
+
+
+    public virtual void OnTouchPadPressed(InputEventArgs e) {
+        if (TouchPadPressed != null) {
+            TouchPadPressed(e);
+        }
+    }
+
+    public virtual void OnTouchPadPressedDown(InputEventArgs e) {
+        if (TouchPadPressDown != null) {
+            TouchPadPressDown(e);
+        }
+    }
+
+    public virtual void OnTouchPadPressedUp(InputEventArgs e)
+    {
+        if (TouchPadPressUp != null)
+        {
+            TouchPadPressUp(e);
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+
+        if (trackedObj == null) {
+            trackedObj = gameObject.AddComponent<SteamVR_TrackedObject>();
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // Check for trigger related input...
+        if (device.GetPress(SteamVR_Controller.ButtonMask.Trigger)) {
+            //Debug.Log("Trigger is being pressed!" + contTypeString);
+            isTriggerPressed = true;
+        }
+        if (device.GetPressDown(SteamVR_Controller.ButtonMask.Trigger)) {
+            //Debug.Log("Trigger Press Down!" + contTypeString);
+            InputEventArgs args = new InputEventArgs();
+            args.controller = this.device;
+            args.controllerType = this.controllerType;
+            OnTriggerDown(args);
+        }
+        if (device.GetPressUp(SteamVR_Controller.ButtonMask.Trigger)) {
+            //Debug.Log("Trigger Press Up!" + contTypeString);
+            InputEventArgs args = new InputEventArgs();
+            args.controller = this.device;
+            args.controllerType = this.controllerType;
+            OnTriggerUp(args);
+            isTriggerPressed = false;
+        }
+
+        // Check for touchpad related input...
+        // First we check for touches
+        if (device.GetTouch(SteamVR_Controller.ButtonMask.Touchpad)) {
+            //Debug.Log("Touchpad is being touched!" + contTypeString);
+            isTouchPadTouched = true;
+        }
+        if (device.GetTouchUp(SteamVR_Controller.ButtonMask.Touchpad)) {
+            //Debug.Log("Touchpad Touch up!" + contTypeString);
+            isTouchPadTouched = false;
+            InputEventArgs args = new InputEventArgs();
+            args.controller = this.device;
+            args.controllerType = this.controllerType;
+            OnTouchPadTouchedUp(args);
+        }
+        if (device.GetTouchDown(SteamVR_Controller.ButtonMask.Touchpad)) {
+            //Debug.Log("Touchpad Touch down!" + contTypeString);
+            InputEventArgs args = new InputEventArgs();
+            args.controller = this.device;
+            args.controllerType = this.controllerType;
+            OnTouchPadTouchedDown(args);
+        }
+
+        // Then we check for presses
+        if (device.GetPress(SteamVR_Controller.ButtonMask.Touchpad)) {
+            /*
+            Vector2 touch = device.GetAxis(Valve.VR.EVRButtonId.k_EButton_Axis0);
+
+            Debug.Log("Touchpad pressed at: " + touch + ", " + contTypeString);
+
+            InputEventArgs args = new InputEventArgs();
+            args.padX = touch.x;
+            args.padY = touch.y;
+            OnTouchPadPressed(args);
+            */
+            isTouchPadPressed = true;
+        }
+        if (device.GetPressDown(SteamVR_Controller.ButtonMask.Touchpad)) {
+            Vector2 touch = device.GetAxis(Valve.VR.EVRButtonId.k_EButton_Axis0);
+
+            Debug.Log("Touchpad pressed down at: " + touch + ", " + contTypeString);
+
+            InputEventArgs args = new InputEventArgs();
+            args.controller = this.device;
+            args.controllerType = this.controllerType;
+            args.padY = touch.y;
+            args.padX = touch.x;
+            OnTouchPadPressedDown(args);
+        }
+        if (device.GetPressUp(SteamVR_Controller.ButtonMask.Touchpad))
+        {
+            Vector2 touch = device.GetAxis(Valve.VR.EVRButtonId.k_EButton_Axis0);
+
+            Debug.Log("Touchpad pressed up at: " + touch + ", " + contTypeString);
+
+            InputEventArgs args = new InputEventArgs();
+            args.controller = this.device;
+            args.controllerType = this.controllerType;
+            args.padY = touch.y;
+            args.padX = touch.x;
+            OnTouchPadPressedUp(args);
+            isTouchPadPressed = false;
+        }
+    }
+}
---
+++ Assets/Scripts/Teleporter.cs
@@ -0,0 +1,200 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporter : MonoBehaviour {
+
+    public Color color;
+    public float thickness = 0.002f;
+    public Transform player;
+    public GameObject pointerIndicatorPrefab;
+
+    private GameObject pointer;
+    private GameObject holder;
+
+    private bool isActive = false;
+
+    private GameObject pointerIndicator;
+    private MeshRenderer pointerIndicatorMeshRenderer;
+    private Transform pointerTransform;
+    private Transform pointerIndicatorTransform;
+
+    private Transform currContact = null;
+    private bool isCurrContact = false;
+
+    private ControllerInputManager m_input_manager;
+
+    private Transform m_transform;
+
+    private void Awake()
+    {
+        m_input_manager = GetComponent<ControllerInputManager>();
+    }
+
+    private void OnEnable()
+    {
+        m_input_manager.TouchPadPressDown += new InputEventHandler (ActivateTeleporter);
+        m_input_manager.TouchPadPressUp += new InputEventHandler(DeactivateTeleporter);
+    }
+
+    private void OnDisable()
+    {
+        m_input_manager.TouchPadPressDown -= new InputEventHandler(ActivateTeleporter);
+        m_input_manager.TouchPadPressUp -= new InputEventHandler(DeactivateTeleporter);
+    }
+
+    // Event Handlers...
+    private void ActivateTeleporter(InputEventArgs e)
+    {
+        Debug.Log("should activate teleporter!");
+        // activate
+        if (pointer != null)
+        {
+            pointer.SetActive(true);
+            isActive = true;
+
+            if (currContact != null)
+            {
+                EnablePointerIndicator();
+            }
+        }
+    }
+
+    private void DeactivateTeleporter(InputEventArgs e)
+    {
+        // teleport to the location
+        player.position = pointerIndicator.transform.position;
+
+        // deactivate
+        if (pointer != null)
+        {
+            pointer.SetActive(false);
+            DisablePointerIndicator();
+        }
+        isActive = false;
+    }
+
+
+    // Use this for initialization
+    void Start()
+    {
+        holder = new GameObject();
+        holder.transform.parent = this.transform;
+        holder.transform.localPosition = Vector3.zero;
+        holder.transform.localRotation = Quaternion.identity;
+
+        pointer = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        pointer.name = "Pointer";
+        pointer.transform.parent = holder.transform;
+        pointer.transform.localScale = new Vector3(thickness, thickness, 100f);
+        pointer.transform.localPosition = new Vector3(0f, 0f, 50f);
+        pointer.transform.localRotation = Quaternion.identity;
+
+        Material newMaterial = new Material(Shader.Find("Unlit/Color"));
+        newMaterial.SetColor("_Color", color);
+        pointer.GetComponent<MeshRenderer>().material = newMaterial;
+
+        if (pointerIndicatorPrefab != null)
+        {
+            pointerIndicator = GameObject.Instantiate(pointerIndicatorPrefab);
+            pointerIndicatorMeshRenderer = pointerIndicator.GetComponent<MeshRenderer>();
+            pointerIndicatorMeshRenderer.enabled = false;
+            pointerIndicatorTransform = pointerIndicator.transform;
+        }
+
+        pointerTransform = pointer.transform;
+
+        m_transform = GetComponent<Transform>();
+
+        BoxCollider collider = pointer.GetComponent<BoxCollider>();
+
+        if (collider)
+        {
+            UnityEngine.Object.Destroy(collider);
+        }
+
+        if (!isActive)
+        {
+            pointer.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isActive)
+        {
+            return;
+        }
+
+        float dist = 100f;
+        float thick = thickness;
+
+        Ray raycast = new Ray(m_transform.position, m_transform.forward);
+        RaycastHit hit;
+        bool bHit = Physics.Raycast(raycast, out hit);
+
+        if (bHit)
+        {
+            Transform hitTransform = hit.transform;
+
+            // handle new hit...
+            if (currContact == null || (currContact != hitTransform))
+            {
+
+                if (currContact != null)
+                {
+                    // OnPointerOut();
+                }
+
+                currContact = hitTransform;
+                isCurrContact = true;
+
+                // responder = currContact.gameObject.GetComponent(typeof(IPointerResponder)) as IPointerResponder;
+                // OnPointerIn();
+                EnablePointerIndicator();
+            }
+
+            // handle hitting the same object...
+            if (currContact != null && currContact == hitTransform)
+            {
+                if (pointerIndicatorTransform != null)
+                {
+                    pointerIndicatorTransform.position = hit.point;
+                }
+            }
+            if (hit.distance < 100f)
+            {
+                dist = hit.distance;
+            }
+        }
+        else
+        {
+            if (currContact != null)
+            {
+                isCurrContact = false;
+                currContact = null;
+                DisablePointerIndicator();
+            }
+        }
+
+        pointer.transform.localScale = new Vector3(thick, thick, dist);
+        pointer.transform.localPosition = new Vector3(0f, 0f, dist / 2f);
+    }
+
+    private void DisablePointerIndicator()
+    {
+        if (pointerIndicatorMeshRenderer != null)
+        {
+            pointerIndicatorMeshRenderer.enabled = false;
+        }
+    }
+
+    private void EnablePointerIndicator()
+    {
+        if (pointerIndicatorMeshRenderer != null)
+        {
+            pointerIndicatorMeshRenderer.enabled = true;
+        }
+    }
+}
