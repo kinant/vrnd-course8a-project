c49bc4fa3fb97a521cc5fdd2bfcc0d898d9799e1
Kinan Turman <kinan.t@gmail.com>
2017-04-14 04:04:58 -0600

    Grab Objects
    Controllers can now be used to grab objects.

---
+++ Assets/Scripts/ControllerGrabObject.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerGrabObject : MonoBehaviour
+{
+    private GameObject collidingObject;
+    private GameObject objectInHand;
+    private ControllerInputManager m_input_manager;
+
+    private void Awake()
+    {
+        m_input_manager = GetComponent<ControllerInputManager>();
+        // trackedObj = GetComponent<SteamVR_TrackedObject>();
+    }
+
+    private void SetCollidingObject(Collider col)
+    {
+
+        if (collidingObject || !col.GetComponent<Rigidbody>())
+        {
+            return;
+        }
+
+        collidingObject = col.gameObject;
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        SetCollidingObject(other);
+    }
+
+    public void OnTriggerStay(Collider other)
+    {
+        SetCollidingObject(other);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (!collidingObject)
+        {
+            return;
+        }
+
+        collidingObject = null;
+    }
+
+    private void GrabObject()
+    {
+        objectInHand = collidingObject;
+        collidingObject = null;
+
+        var joint = AddFixedJoint();
+        joint.connectedBody = objectInHand.GetComponent<Rigidbody>();
+    }
+
+    private FixedJoint AddFixedJoint()
+    {
+        FixedJoint fx = gameObject.AddComponent<FixedJoint>();
+        fx.breakForce = 20000;
+        fx.breakTorque = 20000;
+        return fx;
+    }
+
+    private void ReleaseObject()
+    {
+
+        if (GetComponent<FixedJoint>())
+        {
+            GetComponent<FixedJoint>().connectedBody = null;
+            Destroy(GetComponent<FixedJoint>());
+
+            objectInHand.GetComponent<Rigidbody>().velocity = m_input_manager.controller.velocity;
+            objectInHand.GetComponent<Rigidbody>().angularVelocity = m_input_manager.controller.angularVelocity;
+        }
+
+        objectInHand = null;
+    }
+
+    private void Update()
+    {
+        if (m_input_manager.controller.GetHairTriggerDown())
+        {
+            if (collidingObject)
+            {
+                GrabObject();
+            }
+        }
+
+        if (m_input_manager.controller.GetHairTriggerUp())
+        {
+
+            if (objectInHand)
+            {
+                ReleaseObject();
+            }
+        }
+    }
+}
