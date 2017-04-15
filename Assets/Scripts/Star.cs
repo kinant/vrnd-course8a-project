b1a275e6d591cc48f754034bb2cdd8e04eec6406
Kinan Turman <kinan.t@gmail.com>
2017-04-14 20:55:53 -0600

    Star Script
    Created the star script and the level manager script. Will complete both.

---
+++ Assets/Scripts/Star.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Star : MonoBehaviour {
+
+    public float rotationSpeed = 95f;
+
+    private Transform m_transform;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("BALL HAS HIT STAR");
+        Destroy(gameObject);
+    }
+
+    // Use this for initialization
+    void Start () {
+        m_transform = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        m_transform.Rotate(new Vector3(0f, rotationSpeed * Time.deltaTime, 0f));
+	}
+}
