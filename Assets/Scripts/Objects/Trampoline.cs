9cd2606304ccf3f39c7dab2757392e7ad4dc57dd
Kinan Turman <kinan.t@gmail.com>
2017-04-18 18:33:51 -0600

    Comments
    Tiny comment changes

--- Assets/Scripts/Objects/Trampoline.cs
+++ Assets/Scripts/Objects/Trampoline.cs
@@ -2,10 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// Trampline
+// This class is used to spawn the trampoline at the appropiate rotation. 
+// For some reason it does not spawn at the required rotation even when rotating the prefab.
 public class Trampoline : MonoBehaviour {
 
     private void Awake()
     {
+        // rotate it!
         transform.Rotate(-90f, 0f, 0f);
     }
 }
